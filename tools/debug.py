1a73f91722222b569c4d5bf2851d5c3ff53815d4
Janemwaura <mwaura461@gmail.com>
2024-02-04 22:32:58 +0300

    complete

--- tools/debug.py
+++ tools/debug.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
-import ipdb;
+import sys
+sys.path.append('lib') 
+import ipdb
 
+from Author import Author
+from Magazine import Magazine
+from Article import Article
 
 if __name__ == '__main__':
-#  WRITE YOUR TEST CODE HERE ###
+   
 
+    author1 = Author("Wairimu Mwaura")
+    author2 = Author("Tasha Wangari")
+    
+    ipdb.set_trace()
+    
+    magazine1 = Magazine("Story Hot", "True love")
+    magazine2 = Magazine("Spice FM", "Entertainment")
 
+   
+    ipdb.set_trace()
 
+    article1 = author1.add_article(magazine1, "First Magazine")
+    article2 = author2.add_article(magazine1, "The Trending One")
+    article3 = author1.add_article(magazine2, "Truly True")
 
+    
+    print("Authors:")
+    for author in Author.all():
+        print(author.name())
 
+    print("\nMagazines:")
+    for magazine in Magazine.all():
+        print(f"{magazine.name()} - {magazine.category()}")
 
+    print("\nArticles:")
+    for article in Article.all():
+        print(f"{article.title()} by {article.author().name()} in {article.magazine().name()}")
 
-
-
-# DO NOT REMOVE THIS
+    # DO NOT REMOVE THIS
     ipdb.set_trace()
